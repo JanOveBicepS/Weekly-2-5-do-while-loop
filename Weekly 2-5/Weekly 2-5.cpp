0e2b7b5343ca3caac2bd3054639b57d3726e5284
JanOveBicepS <olemr16@gmail.com>
2020-09-14 17:21:56 +0200

    Add project files.

---
+++ Weekly 2-5/Weekly 2-5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+int main() {
+	int i = 5;
+	int x = 4;
+	int count{ 5 };
+	do
+	{
+		std::cout << i << " ";
+		i++;
+		
+		i += x;
+		++count;
+	} while (i <= 50); {
+	
+		
+	}
+
+
+}
